56a1e9dcf0aaa8654838c577131139f7cf52a484
OSERHIRDEV <oserhir10@gmail.com>
2022-08-18 00:00:26 +0100

    generate Random Password

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let result = document.getElementById("result");
 let length = document.getElementById("length");
+
 let uppercase = document.getElementById("uppercase");
 let lowercase = document.getElementById("lowercase");
 let numbers = document.getElementById("numbers");
@@ -20,8 +21,32 @@ let Num = ArraysFromLowToHight(48, 57);
 let Syn = ArraysFromLowToHight(33, 38);
 
 generate.addEventListener("click", () => {
-  result.innerText = generateRandomPassword();
+  let initialPass = [];
+
+  if (uppercase.checked) {
+    initialPass = [...initialPass, ...Ucase];
+  }
+  if (lowercase.checked) {
+    initialPass = [...initialPass, ...Lcase];
+  }
+  if (numbers.checked) {
+    initialPass = [...initialPass, ...Num];
+  }
+  if (symbols.checked) {
+    initialPass = [...initialPass, ...Syn];
+  }
+
+  let Password = "";
+  for (let i = 0; i < Number(length.value); i++) {
+    Password += randomPassword(initialPass);
+  }
+  result.innerText = Password;
 });
+
+let randomPassword = (initialPass) => {
+  let RandomNumber = Math.floor(Math.random() * initialPass.length);
+  return String.fromCharCode(initialPass[RandomNumber]);
+};
 
 let generateRandomPassword = () => {
   let L = Number(length.value);
@@ -35,5 +60,3 @@ let generateRandomPassword = () => {
 let randomPass = (min, max) => {
   return String.fromCharCode(min + Math.floor(Math.random() * (max - min + 1)));
 };
-
-console.log(String.fromCharCode(118));
